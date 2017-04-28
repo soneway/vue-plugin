2c494f5bfdba854b562206d47195109a6fafa350
宋均均 <sjj107294@alibaba-inc.com>
2017-04-28 17:48:30 +0800

    feat: 添加pc端触摸兼容

--- src/entry/index.js
+++ src/entry/index.js
@@ -1,3 +1,9 @@
 import Index from '../view/Index.vue';
 Index.el = '#root';
+// pc触摸事件兼容
+if (!('ontouchend' in document)) {
+  const scriptEl = document.createElement('script');
+  scriptEl.src = 'http://soneway.github.io/js/desktouch.js';
+  document.head.appendChild(scriptEl);
+}
 export default new Vue(Index);
