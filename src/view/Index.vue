3d7eb4bf19f6d0f2f8f36199e65d1e5c0d62874a
宋均均 <sjj107294@alibaba-inc.com>
2017-04-30 00:10:26 +0800

    feat: 添加loading样式

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -24,7 +24,13 @@
         'http://img.gd.sohu.com/public/images/2014-07-05/75/53b79a22aa37f.jpg',
         'http://img.gd.sohu.com/public/images/2014-07-05/78/53b7cf18a9dae.png',
         'http://img.gd.sohu.com/public/images/2014-07-05/13/53b7aed8da8f7.jpg',
-        'http://img.gd.sohu.com/public/images/2014-07-05/76/53b7f58e12fb9.jpg'
+        'http://img.gd.sohu.com/public/images/2014-07-05/76/53b7f58e12fb9.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/97/53b7f46f5488f.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/76/53b7f446e7fee.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/37/53b7f269dd1ff.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/58/53b7f62dc97ac.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/89/53b7f5f0398d2.jpg',
+        'http://img.gd.sohu.com/public/images/2014-07-05/98/53b7f5e7df185.jpg'
       ];
       carousel.$on('slide', function(i) {
         // console.log(this, i);
