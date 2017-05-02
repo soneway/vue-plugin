62975d49a6ff8c6a03ee4770397658cbdb22efc6
宋均均 <sjj107294@alibaba-inc.com>
2017-05-02 22:57:19 +0800

    feat: 样式及优化代码

--- src/view/UcGallery.vue
+++ src/view/UcGallery.vue
@@ -35,12 +35,49 @@
         height: 100%;
         overflow: auto;
         font-size: 14px;
+        color: #333;
+        line-height: 150%;
+        background-image: linear-gradient(to bottom, rgba(#000, 0.05) 60%, #fff 0%);
 
         .img {
             display: block;
             max-width: 100%;
-            max-height: 50%;
+            max-height: 60%;
+            min-height: 200px;
             margin: 0 auto;
+        }
+
+        .imgInfo {
+            padding: 10px;
+            background: #fff;
+        }
+
+        .indicator {
+            b {
+                font-size: 20px;
+            }
+        }
+
+        .title {
+            font-size: 18px;
+            padding: 10px 0;
+        }
+
+        .subTitle {
+            color: #666;
+            line-height: 100%;
+
+            & > span {
+                &:not(:first-of-type) {
+                    border-left: 1px solid #666;
+                    margin-left: 10px;
+                    padding: 0 10px;
+                }
+            }
+        }
+
+        .desc {
+            padding: 10px 0;
         }
     }
 </style>
@@ -61,8 +98,8 @@
                 <div class="imgInfo">
                     <p class="indicator"><b>${index + 1}</b>/${dataList.length}</p>
                     <h3 class="title">${titleInfo.title}</h3>
-                    <p><span class="source">${titleInfo.source}</span><span class="time">${titleInfo.time}</span></p>
-                    <p>${itemData.desc}</p>
+                    <p class="subTitle"><span class="source">${titleInfo.source}</span><span class="time">${titleInfo.time}</span></p>
+                    <p class="desc">${itemData.desc}</p>
                 </div>
             </div>`;
       };
