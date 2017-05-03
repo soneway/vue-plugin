2662aa01d4184f46efb76a44bd817242dfb51c2e
宋均均 <sjj107294@alibaba-inc.com>
2017-05-03 10:15:40 +0800

    feat: 优化样式

--- src/view/UcGallery.vue
+++ src/view/UcGallery.vue
@@ -11,10 +11,12 @@
     * {
         margin: 0;
         padding: 0;
+        box-sizing: border-box;
     }
 
     body {
         -webkit-overflow-scrolling: touch;
+        font-family: sans-serif;
     }
 
     .wrapper {
@@ -31,13 +33,14 @@
     }
 
     .imgWrap {
+        $padding: 15px;
         width: 100%;
         height: 100%;
         overflow: auto;
-        font-size: 14px;
-        color: #333;
+        font-size: 15px;
+        color: #999;
         line-height: 150%;
-        background-image: linear-gradient(to bottom, rgba(#000, 0.05) 60%, #fff 0%);
+        background-image: linear-gradient(to bottom, rgba(#000, 0.05) 61%, #fff 0%);
 
         .img {
             display: block;
@@ -48,36 +51,50 @@
         }
 
         .imgInfo {
-            padding: 10px;
+            padding: $padding;
             background: #fff;
+            min-height: 40%;
         }
 
+        /*页码指示*/
         .indicator {
-            b {
-                font-size: 20px;
+            font-weight: 100;
+
+            span {
+                font-size: 30px;
+                color: #333;
             }
         }
 
+        /*标题*/
         .title {
-            font-size: 18px;
-            padding: 10px 0;
+            font-size: 20px;
+            padding: $padding 0;
+            color: #333;
         }
 
+        /*来源和日期*/
         .subTitle {
-            color: #666;
+            $color: #bbb;
+            color: $color;
+            font-size: 14px;
             line-height: 100%;
 
             & > span {
+                display: inline-block;
+
                 &:not(:first-of-type) {
-                    border-left: 1px solid #666;
-                    margin-left: 10px;
-                    padding: 0 10px;
+                    border-left: 1px solid $color;
+                    margin-left: 12px;
+                    padding: 0 12px;
                 }
             }
         }
 
+        /*描述信息*/
         .desc {
-            padding: 10px 0;
+            padding: $padding 0;
+            text-indent: 2em;
         }
     }
 </style>
@@ -96,8 +113,8 @@
         return itemData && `<div class="imgWrap">
                 <img class="img" src="${itemData.img}"/>
                 <div class="imgInfo">
-                    <p class="indicator"><b>${index + 1}</b>/${dataList.length}</p>
-                    <h3 class="title">${titleInfo.title}</h3>
+                    <p class="indicator"><span>${index + 1}</span>/${dataList.length}</p>
+                    <p class="title">${titleInfo.title}</p>
                     <p class="subTitle"><span class="source">${titleInfo.source}</span><span class="time">${titleInfo.time}</span></p>
                     <p class="desc">${itemData.desc}</p>
                 </div>
