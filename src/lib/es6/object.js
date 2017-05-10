5f3040b3a6870cd91c4dca33312d70b03910a791
宋均均 <sjj107294@alibaba-inc.com>
2017-05-10 19:20:49 +0800

    feat: 优化

--- src/lib/es6/object.js
+++ src/lib/es6/object.js
@@ -1,6 +1,6 @@
 // Object.assign
 if (!Object.assign) {
-  Object.assign = (obj) => {
+  Object.assign = function(obj) {
     [].slice.call(arguments, 1).forEach((item) => {
       Object.keys(item).forEach((key) => {
         obj[key] = item[key];
