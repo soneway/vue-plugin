4ac8ec464c12a632a55b20fa0b3acd5da0d42ad1
宋均均 <sjj107294@alibaba-inc.com>
2017-05-14 15:18:50 +0800

    feat: request.js

--- src/lib/request.js
+++ src/lib/request.js
@@ -4,6 +4,7 @@ import 'regenerator-runtime/runtime';
 
 const origin = 'https://m.uczzd.cn';
 
+// get请求函数,对应jsonp
 function get(pathname, data) {
   return new Promise((resolve) => {
     jsonp.get({
@@ -13,18 +14,23 @@ function get(pathname, data) {
         resolve(rs);
       },
       error() {
-        resolve(null);
+        resolve();
       }
     });
   });
 }
 
+// post请求函数
 function post(opts) {
 
 }
 
+// 获取图片数据
 async function getImgs() {
   const rs = await get('/iflow/api/v1/channel/100?method=new');
+  if (!rs) {
+    return;
+  }
   const articles = rs.data.articles || {};
   return Object.keys(articles).map((key) => {
     const images = articles[key].images[0] || {};
